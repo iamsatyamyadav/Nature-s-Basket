4d50fa6ececb8d6fe46b43d035c173b2942d7d1b
gauravrai2783 <gauravrai2783@gmail.com>
2022-08-25 23:32:51 +0530

    gaurav rai day-3

---
+++ scripts/nav.js
@@ -0,0 +1,17 @@
+let div = document.getElementById("pop");
+let btn = document.getElementById("bTog");
+
+btn.addEventListener("click", () => {
+    if (div.style.display === "none") {
+        div.style.display = "block"
+    } else {
+        div.style.display = "none"
+    }
+})
+
+function pinChange() {
+
+    let NewPin = document.getElementById("pinVal").value;
+
+    let pin = document.getElementById("pChange").innerText = NewPin
+}
