274f72f428fa9f8f4cc30dba1517c3f258d05cb5
sranbir392 <103633804+sranbir392@users.noreply.github.com>
2022-08-28 00:45:33 +0530

    ranbir day 5

---
+++ scripts/myoffer.js
@@ -0,0 +1,268 @@
+let giftdata=[
+  {
+     img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg" ,
+     
+     name: "E-GIFT Voucher",
+     quantity:"1",
+     price: "5000",
+     // addtocart: "ADD",
+     productID: "r22",
+   },
+   {
+     img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg" ,
+     
+     name: "E-GIFT Voucher",
+     quantity:"1",
+     price: "10000",
+     // addtocart: "ADD",
+     productID: "r23",  
+   },
+  {img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/393c9e50-421d-4583-af61-88c4e66ff873_425x425.jpg"
+  ,
+  name:"The Six Senses Of Indulgence Hamper",
+  quantity:"1",price:"3499",
+  peoductid:"r1",
+} ,
+{img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/d3f189fd-2da7-4f60-a87d-3f6adb163fdd_425x425.jpg",
+name:"The Gluten Free Box",
+quantity:"1",
+price:"4899",
+peoductid:"r2",
+},
+{img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg", 
+name:"The Chocoholic",
+quantity:"1",
+price:"2249",
+productid:"r3",
+},
+{img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg", 
+name:"The Caffeine Addict",
+quantity:"1",
+price:"6999",
+productid:"r4" ,
+},
+
+{img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg", 
+name:"The Tray Of Six Senses",
+quantity:"1",
+price:"1149",
+productid:"r5",
+},
+
+{img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg", 
+name:"Conscious Indulgence Box",
+quantity:"1",
+price:"2149",
+productid:"r6", 
+},
+
+{img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg", 
+name:"The Paresh Maity Hamper",
+quantity:"1r3",
+price:"6549",
+productid:"r7" , 
+},
+
+{img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg", 
+name:"The Exotic World Food Trunk",
+quantity:"1",
+price:"9699",
+productid:"r8",  
+},
+
+{img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/589a65d7-b02e-43a5-b416-1e41b6556453_425x425.jpg", 
+name:"The Midnight Stash",
+quantity:"1",
+price:"899",
+productid:"r9",
+  
+
+},
+{img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/e1a82cda-72a5-4359-9274-ab397dce0098_425x425.jpg", 
+name:"The Exotic World Food Trunk",
+quantity:"1",
+price:"9699",
+productid:"r10",
+},
+{img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/00b01a2b-5669-43a2-b908-a32ca8992abe_425x425.jpg", 
+name:"Good snacker",
+quantity:"1",
+price:"1200",
+productid:"r11",
+},
+
+{img:"https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/12120c2e-1018-481c-a34f-129ddfe43434_425x425.jpg", 
+
+name:"Little Nibbles",
+quantity:"1",
+price:"2499",
+productid:"r12",
+},
+{
+   
+  img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg" ,
+  // 
+  name: "E-GIFT Voucher",
+  quantity:"1",
+  price: "50000 ",
+  // addtocart: "ADD",
+  productID: "r13",
+  },
+{
+
+  img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg" ,
+  name: "E-GIFT Voucher",
+  quantity:"1",
+  price: "1000 ",
+  // addtocart: "ADD",
+  productID: "r14",
+},
+
+{
+  img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg" ,
+  name: "E-GIFT Voucher",
+  quantity:"1",
+  price: "2000",
+  // addtocart: "ADD",
+  productID: "r15",
+},
+
+{
+  img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/5179afc6-856c-428c-8ff7-41ddcce41710_425x425.jpg" ,
+  
+  name: "E-GIFT Voucher",
+  quantity:"1",
+  price: "3000 ",
+  // addtocart: "ADD",
+  productID: "r16",
+  
+},
+
+{
+  img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/589a65d7-b02e-43a5-b416-1e41b6556453_425x425.jpg" ,
+  
+  name: "Embrace The Far East Hamper",
+  quantity:"1",
+  price: "2700",
+  // addtocart: "ADD",
+  productID: "r17",  
+},
+{
+  img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/e1a82cda-72a5-4359-9274-ab397dce0098_425x425.jpg" ,
+  name: "The Luxury World Food Trunk Handpic...",
+  quantity:"1",
+  price: "1700",
+  // addtocart: "ADD",
+  productID: "r18",  
+},
+{
+
+  img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/00b01a2b-5669-43a2-b908-a32ca8992abe_425x425.jpg" ,
+  name: "Food For Throught Hamper",
+  quantity:"1",
+  price: "1500",
+  // addtocart: "ADD",
+  productID: "r19",    
+},
+{
+
+  img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/12120c2e-1018-481c-a34f-129ddfe43434_425x425.jpg" ,
+  name: "Tune Up Immune",
+  quantity:"1",
+  price: "1000",
+  // addtocart: "ADD",
+  productID: "r20",  
+
+},
+{
+
+  img: "https://d1z88p83zuviay.cloudfront.net/ProductVariantThumbnailImages/20f9c9c5-f4c0-4bc5-9a04-47673f98e70b_425x425.jpg" ,
+  name: "The Vitamin C Hamper",
+  quantity:"1",
+  price: "1500",
+  // addtocart: "ADD",
+  productID: "r21",    
+}
+]
+let arrdata=JSON.parse(localStorage.getItem("cart_Data"))||[]
+display(giftdata)
+function display(giftdata){
+ document.querySelector("#boundaryy").innerHTML=""
+   let res=giftdata.forEach(function(el){
+       let divs=document.createElement("div");
+       let inner1=document.createElement("p");
+       inner1.innerText="DELIVERY IN 90 MINUTES";
+     //   let inner2=document.createElement("p");
+     //   inner2.innerText=""
+       let imgs=document.createElement("img");
+       imgs.src=el.img;
+       imgs.setAttribute("class","imagegift")
+       let texts1=document.createElement("p");
+       texts1.innerText=el.name;
+       let texts2=document.createElement("p");
+       texts2.innerText=el.price;
+       let btn=document.createElement("img");
+       btn.src="https://www.naturesbasket.co.in/Images/CartAddBtn.PNG";
+       btn.setAttribute("class","cartthing")
+       btn.addEventListener("click",function(){
+         console.log(el)
+         arrdata.push(el)
+         console.log(arrdata)
+         localStorage.setItem("cartmei",JSON.stringify(arrdata))
+         alert("Product added to cart")
+       })
+       divs.append(imgs,inner1,texts1,texts2,btn);
+        document.querySelector("#boundaryy").append(divs);
+
+     
+   })
+ }
+
+   let productbox=document.getElementById("boundaryy");
+document.querySelector("#sort").addEventListener("click",mysort)
+function mysort(){
+   productbox.innerHTML="";
+   let number=document.querySelector("#sort").value
+   if(number==1){
+       giftdata.sort(function(a,b){
+           return a.price-b.price
+       })
+       // console.log(giftdata)
+       
+       display(giftdata);
+
+   }
+   if(number==2){
+       giftdata.sort(function(a,b){
+           return b.price-a.price
+       })
+       display(giftdata);
+   }
+   if(number==0){
+       display(giftdata);
+   }
+}
+
+       function gofilter(){
+       let selectedd=+(document.querySelector("#fill").value)
+       console.log(selectedd);
+       if(selectedd!==0){
+       let res=giftdata.filter(function(el,){
+          el.price=Number(el.price)
+             // console.log(typeof(el.price))
+         // if(selectedd===0){
+         //   display(giftdata)
+         //   // window.location.reload()
+         //     }
+             if(selectedd<15000){
+        return (el.price < selectedd && el.price >=(selectedd-5000))
+             }
+             else{ return el.price >=(selectedd-5000)
+             }
+       //  console.log(el.price  selectedd)
+      })
+     //  console.log(res)
+         display(res)
+     }
+      else{display(giftdata)}
+     }
